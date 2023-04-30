35c816367d646a01383a808a6cb7373f2f58e19a
othAs-dev <othmane.developer@gmail.com>
2023-04-30 12:23:38 +0200

    ajout d'une fonctionnalité permettant de trouver un pokemon en fonction de son id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pokemonNg';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    if (!pokemon) {
+      console.error("ca n'existe pas");
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`vous avez choisi ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }
+  }
+  handleClick(pokemonName?: string) {
+    alert(`Tu as choisi ${pokemonName}`);
+  }
 }
