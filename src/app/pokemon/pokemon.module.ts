253af4480225281c60588ad439c61ace2ecc201a
othAs-dev <othmane.developer@gmail.com>
2023-05-12 11:17:10 +0200

    fix design

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -21,7 +21,7 @@ import {AuthGuard} from "../auth.guard";
 const pokemonRoutes: Routes = [
   { path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [AuthGuard] },
   {path: 'pokemon/add', component: AddPokemonComponent, canActivate: [AuthGuard]},
-  { path: 'pokemons', component: PokemonListComponent },
+  { path: 'pokemons', component: PokemonListComponent, canActivate: [AuthGuard] },
   { path: 'pokemon/:id', component: DetailPokemonComponent, canActivate: [AuthGuard] },
 ];
 
